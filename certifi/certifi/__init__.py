a9455db74c7c9b65e952242c2d358cb759a52e43
Mark Haley <mark.haley@tignis.com>
2024-12-20 15:17:37 -0500

    fix: make certifi work with python v3.8

--- certifi/certifi/__init__.py
+++ certifi/certifi/__init__.py
@@ -1,5 +1,14 @@
-from importlib.resources import files
+from sys import version_info
 import certifi
 
-def where():
-    return str(files(certifi)/"root_store.certs")
+if version_info < (3, 9):
+    from importlib.resources import path
+
+    def where():
+        with path(certifi, "root_store.certs") as p:
+            return str(p)
+else:
+    from importlib.resources import files
+
+    def where():
+        return str(files(certifi)/"root_store.certs")
